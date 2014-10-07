2143cfcbddd023fdfc4adb01cf2307c71e429fd5
Anton Ishkov <iskov.anton@yandex.com>
2014-10-07 11:00:32 +0200

    Add debug mode switcher

--- app/code/community/Seamless/SEQR/Block/Payment.php
+++ app/code/community/Seamless/SEQR/Block/Payment.php
@@ -24,4 +24,11 @@ class Seamless_SEQR_Block_Payment extends Mage_Core_Block_Template {
             Mage::getStoreConfig('payment/seqr/debug') ? 'SEQR-DEBUG://' : 'SEQR://',
             $qrcode = $this->getQrCode());
     }
+
+    protected function getWebPluginUrl() {
+        return 'https://cdn.seqr.com/webshop-plugin/js/seqrShop.js'.
+        '#!'.(Mage::getStoreConfig('payment/seqr/debug') ? '' : 'mode=demo').'&injectCSS=true&statusCallback=seqrStatusUpdated&'.
+        'invoiceQRCode='.$this->getQrCode().'&'.
+        'statusURL='.Mage::getBaseUrl().'/seqr/payment/check';
+    }
 }
