85177f2e0714c505dbf16a037c46fb87fefd2277
Anton Ishkov <iskov.anton@yandex.com>
2014-11-19 11:47:36 +0100

    Uninstall script

---
+++ uninstaller.sh
@@ -0,0 +1,6 @@
+rm -rf ./app/code/community/Seamless/SEQR
+rn -rf ./app/design/frontend/base/default/layout/seqr.xml
+rn -rf ./app/design/frontend/base/default/template/seqr
+rn -rf ./app/etc/modules/Seamless_SEQR.xml
+rn -rf ./js/seqr
+rn -rf ./skin/frontend/base/default/css/seqr
