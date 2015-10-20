1ec3362a3cf2e3f756cc463cf7fe4fc6370d4bac
Anton Iskov <anton.iskov@seamless.se>
2015-10-20 12:49:40 +0200

    Add cancel buttons

--- app/code/community/Seamless/SEQR/Block/Payment.php
+++ app/code/community/Seamless/SEQR/Block/Payment.php
@@ -1,34 +1,47 @@
 <?php
 
-class Seamless_SEQR_Block_Payment extends Mage_Core_Block_Template {
+class Seamless_SEQR_Block_Payment extends Mage_Core_Block_Template
+{
+    private $data = null;
 
-    protected function getOrderId() {
-
+    protected function getOrderId()
+    {
         return Mage::registry('orderid');
     }
 
-    protected function getSecondsBeforeCancel() {
-
+    protected function getSecondsBeforeCancel()
+    {
         return intval(Mage::getStoreConfig('payment/seqr/seconds_before_cancel'));
     }
 
-    protected function getQrCode() {
+    protected function getQrCode()
+    {
+        if (! $this->data) {
+            $order = Mage::getModel('sales/order')->load(Mage::registry('orderid'));
+            $this->data = Mage::getSingleton('seqr/invoice')->getAdditionalData($order);
+        }
 
-        $order = Mage::getModel('sales/order')->load(Mage::registry('orderid'));
-        return Mage::getSingleton('seqr/invoice')->getInvoice($order)->invoiceQRCode;
+        if (!$this->data || !$this->data->invoiceQRCode) return null;
+        return $this->data->invoiceQRCode;
     }
 
-    protected function getSeqrUrl() {
-
+    protected function getSeqrUrl()
+    {
         return preg_replace('/^HTTP\:\/\//',
             Mage::getStoreConfig('payment/seqr/debug') ? 'SEQR-DEBUG://' : 'SEQR://',
             $qrcode = $this->getQrCode());
     }
 
-    protected function getWebPluginUrl() {
+    protected function getWebPluginUrl()
+    {
         return 'https://cdn.seqr.com/webshop-plugin/js/seqrShop.js'.
         '#!'.(Mage::getStoreConfig('payment/seqr/debug') ? '' : 'mode=demo').'&injectCSS=true&statusCallback=seqrStatusUpdated&'.
         'invoiceQRCode='.$this->getQrCode().'&'.
         'statusURL='.Mage::getBaseUrl().'/seqr/payment/check';
+    }
+
+    protected function getCancelUrl()
+    {
+        return Mage::getBaseUrl().'seqr/payment/cancel';
     }
 }
